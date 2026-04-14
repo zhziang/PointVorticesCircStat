43fc3a91a4bccc0e6ee4d99e5bb4844188a5d11e
ziang-alpha <zzalpha2024@163.com>
2026-04-14 16:28:25 +0800

    modified project structure.

--- src/postprocess.jl
+++ src/postprocess.jl
@@ -7,15 +7,32 @@ include("utils/postprocess.jl")
 
 basepath = dirname(@__DIR__)
 
-srcFiles = Dict([
-	:VortexPair => h5open(basepath * "/.output/N256H-0.001543.h5", "r");
-	:MaxEntropy => h5open(basepath * "/.output/N256H-0.000917.h5", "r");
-	:Condensate => h5open(basepath * "/.output/N256H0.0.h5", "r")
-])
+srcFiles = h5open(basepath * "/.output/N64H0.0.h5")
 desFile = h5open(basepath * "/postprocess.h5", "w")
 
-ngrid = 256
+function estimate(func, file)
+    circs = read(file["circulations"])
+    samples = Iterators.map(1:1000) do n
+        u = file["pv positions"][:, :, n]
+        func(u, circs)
+    end
+    nsamples = length(samples)
+    mean = sum(samples) ./ nsamples
+    std = sqrt.(sum(x -> (x .- mean) .^ 2, samples) ./ nsamples)
+    return (mean, std)
+end
 
-grid = TwoDGrid(GPU(); nx = ngrid, Lx = 1)
+grid = TwoDGrid(CPU(); nx=256, Lx=1)
 
+function rectCircMoments(ns, width, height, order)
+    res = map(ns) do n
+        estimate(srcFiles) do u, circs
+            ζh = grid.rfftplan * pv2field(u, circs, grid)
+            Γ = rectCirculations(ζh, width * n, height * n, grid)
+            sum(x -> x^order, Γ) / length(Γ)
+        end
+    end
+
+    return getindex.(res, 1), getindex.(res, 2)
+end
 
